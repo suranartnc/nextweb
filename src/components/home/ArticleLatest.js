377cbbe151d3b73b93fd87c7564057733f04dbce
Suranart Niamcome <suranartnc@gmail.com>
2019-04-02 19:39:26 +0700

    Add fetch api utility

--- src/components/home/ArticleLatest.js
+++ src/components/home/ArticleLatest.js
@@ -1,27 +1,36 @@
 import React from 'react'
+import { get } from 'lodash'
 
-ArticleList.defaultProps = {
+// Mock Data
+ArticleLatest.defaultProps = {
   data: [
     {
+      id: 1,
       title: 'Article 1',
       excerpt: 'Excerpt 1',
     },
     {
+      id: 2,
       title: 'Article 2',
       excerpt: 'Excerpt 2',
     },
     {
+      id: 3,
       title: 'Article 3',
       excerpt: 'Excerpt 3',
     },
   ],
 }
 
-export default function ArticleLatest() {
+export default function ArticleLatest({ data }) {
+  if (get(data, 'length', 0) === 0) {
+    return null
+  }
+
   return (
     <section>
       <h2>Latest Articles</h2>
-      <ArticleList />
+      <ArticleList data={data} />
     </section>
   )
 }
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+
+import { fetchAPI } from '@lib/api'
 import ArticleLatest from './ArticleLatest'
 
-export default function HomePage() {
+export default function HomePage({ data }) {
   return (
     <div>
       <Helmet title="Home" />
-      <ArticleLatest />
+      <ArticleLatest data={data} />
     </div>
   )
+}
+
+HomePage.getInitialProps = async () => {
+  const rersult = await fetchAPI({ path: '/articles' })
+  return {
+    data: rersult.data,
+  }
 }
