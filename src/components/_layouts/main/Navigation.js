b570ff7a38647f88672816d095f5c3f41bae1e35
Suranart Niamcome <suranartnc@gmail.com>
2019-04-02 14:58:01 +0700

    Add babel plugin lodash

--- src/components/_layouts/main/Navigation.js
+++ src/components/_layouts/main/Navigation.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Link } from '@router'
+import { get } from 'lodash'
+
+const mainMenus = [
+  {
+    name: 'Home',
+    route: 'home',
+  },
+  {
+    name: 'About',
+    route: 'about',
+  },
+]
 
 export default function Navigation() {
+  const menus = get(mainMenus, '', [])
+
   return (
     <div>
-      <Link to="home">Home</Link>
-      <Link to="about">About</Link>
+      {menus.map(menu => (
+        <Link key={menu.name} to={menu.route}>
+          {menu.name}
+        </Link>
+      ))}
     </div>
   )
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 const nextRoutes = require('next-routes')
-const { get } = require('lodash')
 const routes = nextRoutes()
 
 const main = require('./routes/_main')
@@ -8,17 +7,6 @@ const allRoutes = [...main]
 
 allRoutes.forEach(function({ name, pattern, page }) {
   routes.add(name, pattern, page)
-})
-
-routes.routes = routes.routes.map(function addOptionsEachRoute(route) {
-  const routeFromSearchAllRoutes = allRoutes.find(function findRouteFromName(
-    r,
-  ) {
-    return r.name === route.name
-  })
-  const options = get(routeFromSearchAllRoutes, 'options')
-  typeof options !== 'undefined' && (route.options = options)
-  return route
 })
 
 module.exports = routes
