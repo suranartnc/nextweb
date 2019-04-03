ff1d555725d368056b934f9cc044741f27545aab
Suranart Niamcome <suranartnc@gmail.com>
2019-04-03 19:58:27 +0700

    Refactor error page

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,26 +1,22 @@
 import React from 'react'
 import { get } from 'lodash'
 
-class Error extends React.Component {
-  static getInitialProps({ res, err }) {
-    const isNotFound = get(res, 'statusCode') === 404
-    const errorCode = get(err, 'statusCode', 500)
-    const statusCode = isNotFound ? 404 : errorCode
-
-    return { statusCode }
-  }
-
-  render() {
-    const { statusCode } = this.props
-
-    return (
-      <p>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
-    )
-  }
+function ErrorPage({ statusCode }) {
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : 'An error occurred on client'}
+    </p>
+  )
 }
 
-export default Error
+ErrorPage.getInitialProps = async function({ res, err }) {
+  const isNotFound = get(res, 'statusCode') === 404
+  const errorCode = get(err, 'statusCode', 500)
+  const statusCode = isNotFound ? 404 : errorCode
+
+  return { statusCode }
+}
+
+export default ErrorPage
