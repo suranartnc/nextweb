9ad0dd28f5eea15261f74ba79b71ebabf1ce1ba1
Suranart Niamcome <suranartnc@gmail.com>
2019-04-09 18:15:43 +0700

    Adopt emotion

--- src/components/_layouts/main/Breadcrumb.js
+++ src/components/_layouts/main/Breadcrumb.js
@@ -1,32 +1,32 @@
 import React, { Fragment } from 'react'
 import { Link } from '@router'
+import styled from '@emotion/styled'
+
+const Nav = styled.nav`
+  font-size: 0.8em;
+  margin-bottom: 30px;
+`
+
+const NavLink = styled.a`
+  display: inline-box;
+  padding: 5px;
+`
 
 export default function Breadcrumb({ data = [] }) {
   return (
-    <nav>
+    <Nav>
       <Link key="Home" to="home">
-        <a>Home</a>
+        <NavLink>Home</NavLink>
       </Link>
 
       {data.map(({ label, route }) => (
         <Fragment key={label}>
           <span>&gt;</span>
           <Link to={route.name} params={route.params}>
-            <a>{label}</a>
+            <NavLink>{label}</NavLink>
           </Link>
         </Fragment>
       ))}
-
-      <style jsx>{`
-        nav {
-          font-size: 0.8em;
-          margin-bottom: 30px;
-        }
-        a {
-          display: inline-box;
-          padding: 5px;
-        }
-      `}</style>
-    </nav>
+    </Nav>
   )
 }
--- src/components/_layouts/main/Navigation.js
+++ src/components/_layouts/main/Navigation.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { Link } from '@router'
 import * as GTM from '@lib/stats/gtm'
 import { userContext } from '@lib/firebase/auth'
+import styled from '@emotion/styled'
 
 const mainMenus = [
   {
@@ -13,6 +14,17 @@ const mainMenus = [
     route: 'about',
   },
 ]
+
+const Nav = styled.nav`
+  margin-bottom: 10px;
+  border-bottom: 1px solid #aaa;
+`
+
+const NavLink = styled.a`
+  display: inline-box;
+  padding: 5px 15px 10px 0px;
+  margin-right: 10px;
+`
 
 export default function Navigation() {
   const userData = useContext(userContext)
@@ -28,34 +40,22 @@ export default function Navigation() {
   }
 
   return (
-    <nav>
+    <Nav>
       {mainMenus.map(menu => (
         <Link key={menu.name} to={menu.route}>
-          <a onClick={trackEvent(menu)}>{menu.name}</a>
+          <NavLink onClick={trackEvent(menu)}>{menu.name}</NavLink>
         </Link>
       ))}
 
       {userData ? (
         <Link key="Account" to="account">
-          <a onClick={trackEvent({ name: 'Account' })}>Account</a>
+          <NavLink onClick={trackEvent({ name: 'Account' })}>Account</NavLink>
         </Link>
       ) : (
         <Link key="Login" to="login">
-          <a onClick={trackEvent({ name: 'Login' })}>Login</a>
+          <NavLink onClick={trackEvent({ name: 'Login' })}>Login</NavLink>
         </Link>
       )}
-
-      <style jsx>{`
-        nav {
-          margin-bottom: 10px;
-          border-bottom: 1px solid #aaa;
-        }
-        a {
-          display: inline-box;
-          padding: 5px 15px 10px 0px;
-          margin-right: 10px;
-        }
-      `}</style>
-    </nav>
+    </Nav>
   )
 }
