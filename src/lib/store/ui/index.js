00d07f5660c1bec9d90d5ab8c27a43690e13f989
Suranart Niamcome <suranartnc@gmail.com>
2019-04-17 13:00:48 +0700

    Remove warnings

--- src/lib/store/ui/index.js
+++ src/lib/store/ui/index.js
@@ -18,7 +18,7 @@ export default class UIStore {
         height: window.innerHeight,
       })
     }
-    listener()
+    setTimeout(listener, 0)
 
     window.addEventListener('resize', listener)
   }
