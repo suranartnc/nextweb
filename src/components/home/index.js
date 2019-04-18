1527c0dee5f8f0a287d0d37128feeafaf698e118
Suranart Niamcome <suranartnc@gmail.com>
2019-04-18 13:33:05 +0700

    Add fetch more usage

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import { Flex, Box } from '@rebass/grid/emotion'
 
 import withPage from '@lib/page/withPage'
-import { getLatestArticles } from '@features/article/data/model'
+import { getLatestArticles, getArticles } from '@features/article/data/model'
 import ArticleLatest from './ArticleLatest'
+
+function MoreArticles({ start, limit }) {
+  const [lastStart, setLastStart] = useState(start)
+  const [data, setData] = useState([])
+
+  const fetchMore = async () => {
+    const articles = await getArticles({ start: lastStart, limit })
+    setLastStart(lastStart + articles.length)
+    setData([...data, ...articles])
+  }
+
+  return (
+    <Fragment>
+      <ArticleLatest data={data} />
+      <button onClick={fetchMore}>Load more</button>
+    </Fragment>
+  )
+}
 
 function HomePage({ articleLatest }) {
   return (
     <Flex flexWrap="wrap">
       <Box width={[1, 2 / 3]} pr={[0, 20]}>
         <ArticleLatest data={articleLatest} />
+        <MoreArticles start={5} limit={5} />
       </Box>
 
       <Box width={[1, 1 / 3]} pl={[0, 20]}>
