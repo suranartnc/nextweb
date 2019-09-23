3bf46728cf201d80be9ede68254b76b79b320b9c
Suranart Niamcome <suranartnc@gmail.com>
2019-09-23 15:06:42 +0700

    Add support for static web

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -11,7 +11,7 @@ function ErrorNotFoundPage({ statusCode = 500 }) {
   return isError400 ? <Error400Page routeMisMatched={true} /> : <Error500Page />
 }
 
-ErrorNotFoundPage.getInitialProps = async function({ res, err }) {
+ErrorNotFoundPage.getInitialProps = async function({ res, err = {} }) {
   return {
     statusCode: res.statusCode === 404 ? res.statusCode : err.statusCode,
   }
