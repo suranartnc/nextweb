7108eda073a0295e9e9a0e1a3866a97f27db6150
Suranart Niamcome <suranartnc@gmail.com>
2019-09-23 12:42:31 +0700

    Add withSchema and withStat

--- src/lib/page/withPage.js
+++ src/lib/page/withPage.js
@@ -6,6 +6,7 @@ import withStats from '@lib/stats/withStats'
 import { withRestrictedRoute } from '@lib/firebase/auth'
 
 import withMeta from './withMeta'
+import withSchema from './withSchema'
 import withLayout from './withLayout'
 import withErrorHandling from './withErrorHandling'
 
@@ -14,6 +15,7 @@ export default function withPage(options = {}) {
     const hocs = [
       withUA,
       withMeta,
+      withSchema,
       withGtmScript,
       withStats,
       withRestrictedRoute(options.restricted),
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import Helmet from 'react-helmet'
 import App from 'next/app'
+import Router from 'next/router'
 import { Provider } from 'mobx-react'
 
 import initMobXStore from '@lib/store'
@@ -11,6 +12,12 @@ export default class MyApp extends App {
   componentDidMount() {
     const WebFont = require('webfontloader')
     WebFont.load(font.config)
+
+    Router.events.on('routeChangeStart', url => {
+      if (window.__NEXT_DATA__.props.isSSR === undefined) {
+        window.__NEXT_DATA__.props.isSSR = false
+      }
+    })
   }
 
   render() {
