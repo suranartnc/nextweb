ce91b545146b40dcae5db6bce65badfe28dc9769
Suranart Niamcome <suranartnc@gmail.com>
2019-09-23 11:37:36 +0700

    Remove HoC for app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,40 @@
 import React, { Fragment } from 'react'
 import Helmet from 'react-helmet'
 import App from 'next/app'
-import { flowRight as compose } from 'lodash'
+import { Provider } from 'mobx-react'
 
-import { withFontLoader } from '@lib/font'
-import { withUA } from '@lib/userAgent'
-import withMobX from '@lib/store/withMobX'
+import initStore from '@lib/store'
+import { getStatic } from '@lib/static'
 import { GlobalStyles } from '@lib/styles'
 
 class MyApp extends App {
+  componentDidMount() {
+    const WebFont = require('webfontloader')
+
+    WebFont.load({
+      custom: {
+        urls: [
+          getStatic('css/fonts.css'),
+          // 'https://fonts.googleapis.com/css?family=Mitr:200,300,500%7CSarabun:400,700&display=swap',
+        ],
+      },
+    })
+  }
+
   render() {
     const { Component, router } = this.props
+    const rootStore = initStore()
 
     return (
       <Fragment>
         <GlobalStyles />
         <Helmet titleTemplate={`%s - nextweb.js`} />
-        <Component {...this.props.pageProps} router={router} />
+        <Provider RootStore={rootStore}>
+          <Component {...this.props.pageProps} router={router} />
+        </Provider>
       </Fragment>
     )
   }
 }
 
-export default compose(
-  withUA,
-  withMobX,
-  withFontLoader,
-)(MyApp)
+export default MyApp
