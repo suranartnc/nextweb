862df9940307b1bb541c3232d09a2d625cad47cf
Suranart Niamcome <suranartnc@gmail.com>
2019-09-24 18:50:54 +0700

    Use real jwt

--- src/pages/api/signIn.js
+++ src/pages/api/signIn.js
@@ -1,12 +1,24 @@
+import jwt from 'jsonwebtoken'
+
+const secret = 'nextweb'
+
 export default function signIn(req, res) {
   const { email, password } = req.body
 
+  let response = null
+  let status = 200
+
   if (email !== 'admin' && password !== 'admin') {
-    res.status(401).json({ message: 'Email or password is not correct.' })
+    status = 401
+    response = { message: 'Email or password is not correct.' }
+  } else {
+    const payload = {
+      displayName: email,
+    }
+
+    const token = jwt.sign(payload, secret)
+    response = { token }
   }
 
-  const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkaXNwbGF5TmFtZSI6IkpvaG4gRG9lIn0.sPK58JizZbQSm-t--cEgifxJkYDt4UGHpBihVha8pJ0'
-
-  res.status(200).json({ token })
+  res.status(status).json(response)
 }
