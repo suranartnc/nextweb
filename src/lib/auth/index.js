4c0f9041cb06d78a260ecb5923fd50172c62076d
Suranart Niamcome <suranartnc@gmail.com>
2019-10-23 17:33:09 +0700

    Move some code out of features

--- src/lib/auth/index.js
+++ src/lib/auth/index.js
@@ -1,5 +1,10 @@
-import React, { useContext } from 'react'
-import { useAuth } from '@features/_auth'
+import React, { useState, useContext, useEffect } from 'react'
+import jwt from 'jsonwebtoken'
+import { get } from 'lodash'
+import { useCookies } from 'react-cookie'
+import { useRouter } from 'next/router'
+
+import { AUTH_COOKIE_NAME } from '@features/_auth'
 
 export const userContext = React.createContext({
   isAuthenticated: undefined,
@@ -29,4 +34,50 @@ export function withAuth(PageComponent) {
   }
 
   return EnhancedPageComponent
+}
+
+// A react hook to collect auth data, then makes app knows the user is now logged in
+function useAuth() {
+  const [cookies, setCookie] = useCookies([AUTH_COOKIE_NAME])
+  const [token, setToken] = useState(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    let token = cookies[AUTH_COOKIE_NAME]
+
+    if (!token) {
+      const { token: tokenFromURL } = getAuthDataFromCallbackURL(router.query)
+
+      if (tokenFromURL) {
+        setCookie(AUTH_COOKIE_NAME, tokenFromURL, {
+          path: '/',
+          expires: new Date(getDataFromToken(tokenFromURL)['exp'] * 1000),
+        })
+      }
+
+      token = tokenFromURL
+    }
+
+    setToken(token)
+  }, [])
+
+  const userData = {
+    isAuthenticated: token === null ? null : !!token,
+    profile: getDataFromToken(token),
+    token,
+  }
+
+  return userData
+}
+
+function getDataFromToken(token) {
+  if (token === null) return ''
+
+  return jwt.decode(token)
+}
+
+function getAuthDataFromCallbackURL(query) {
+  return {
+    token: get(query, 'token', false),
+  }
 }
