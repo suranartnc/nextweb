d003e0a40b39d925dab8acae4ffd61abaa3db188
Suranart Niamcome <suranartnc@gmail.com>
2019-10-23 18:08:29 +0700

    Move cookie max age config to feature

--- src/features/_auth/index.js
+++ src/features/_auth/index.js
@@ -1,6 +1,7 @@
 import { postAPI } from '@lib/api/helpers'
 
 export const AUTH_COOKIE_NAME = 'nextweb-token'
+export const AUTH_COOKIE_MAX_AGE = 60 * 60 // 1 hour
 
 export function signIn({ email, password, redirect }) {
   return postAPI({
--- src/lib/auth/useAuth.js
+++ src/lib/auth/useAuth.js
@@ -1,17 +1,15 @@
 import { useState, useEffect } from 'react'
-import { useCookies } from 'react-cookie'
 import { get } from 'lodash'
 import jwt from 'jsonwebtoken'
 import { useRouter } from 'next/router'
 
-import { AUTH_COOKIE_NAME } from '@features/_auth'
-
-const DEFAULT_TOKEN_MAX_AGE = 60 * 60 // 1 hour
+import { useCookies } from 'react-cookie'
+import { AUTH_COOKIE_NAME, AUTH_COOKIE_MAX_AGE } from '@features/_auth'
 
 export default function useAuth() {
-  const [cookies, setCookie] = useCookies([AUTH_COOKIE_NAME])
   const [token, setToken] = useState(null)
   const router = useRouter()
+  const [cookies, setCookie] = useCookies([AUTH_COOKIE_NAME])
 
   useEffect(() => {
     let token = cookies[AUTH_COOKIE_NAME]
@@ -23,7 +21,7 @@ export default function useAuth() {
         const payload = getDataFromToken(tokenFromURL)
         const expires =
           get(payload, 'exp') ||
-          Math.floor(Date.now() / 1000) + DEFAULT_TOKEN_MAX_AGE
+          Math.floor(Date.now() / 1000) + AUTH_COOKIE_MAX_AGE
 
         setCookie(AUTH_COOKIE_NAME, tokenFromURL, {
           path: '/',
