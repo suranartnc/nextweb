8905014e97ed0dd627c12a893c3ae61d15136081
Suranart Niamcome <suranartnc@gmail.com>
2020-08-11 16:51:53 +0700

    Enable trailingSlash

--- src/next.config.js
+++ src/next.config.js
@@ -9,7 +9,7 @@ const exportPathMap = require('./exportPathMap')
 
 const nextConfig = {
   exportPathMap,
-  exportTrailingSlash: true,
+  trailingSlash: true,
   assetPrefix: process.env.ASSET_PREFIX,
   analyzeBrowser: process.env.BUNDLE_ANALYZE === 'browser',
   bundleAnalyzerConfig: {
