82109a5bc1a67be50818127e4fda8157be3711f2
Suranart Niamcome <suranartnc@gmail.com>
2020-08-13 11:52:30 +0700

    Fix missing layout for error 500 issue

--- src/components/_error/400/index.js
+++ src/components/_error/400/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import Page from '@components/Page'
+
 import NoResource from './NoResource'
 import NotFound from './NotFound'
 
 export default function Error400Page({ routeMisMatched, ...props }) {
-  return routeMisMatched ? <NotFound {...props} /> : <NoResource {...props} />
+  return (
+    <Page>
+      {routeMisMatched ? <NotFound {...props} /> : <NoResource {...props} />}
+    </Page>
+  )
 }
--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import Page from '@components/Page'
 import Error400Page from '@components/_error/400'
 import Error500Page from '@components/_error/500'
 
@@ -9,11 +8,7 @@ import withPageError from '@lib/page/withPageError'
 function ErrorNotFoundPage({ statusCode = 500 }) {
   const isError400 = statusCode >= 400 && statusCode < 500
 
-  return (
-    <Page>
-      {isError400 ? <Error400Page routeMisMatched={true} /> : <Error500Page />}
-    </Page>
-  )
+  return isError400 ? <Error400Page routeMisMatched={true} /> : <Error500Page />
 }
 
 ErrorNotFoundPage.getInitialProps = async function({ res, err = {} }) {
