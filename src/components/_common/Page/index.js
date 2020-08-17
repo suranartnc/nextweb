24670bb75913a65216073f2339efc436697b56a6
Suranart Niamcome <suranartnc@gmail.com>
2020-08-17 13:46:36 +0700

    Extract stats from meta component

--- src/components/_common/Page/index.js
+++ src/components/_common/Page/index.js
@@ -5,6 +5,7 @@ import { Router } from '@router'
 import { useMember } from '@lib/auth'
 import MainLayout from '@components/_layouts/main'
 import Meta from './Meta'
+import Stats from './Stats'
 
 export default function Page({ children, data, metaConfig, options = {} }) {
   const router = useRouter()
@@ -24,6 +25,7 @@ export default function Page({ children, data, metaConfig, options = {} }) {
   return (
     <Fragment>
       <Meta data={data} metaConfig={metaConfig} />
+      <Stats data={data} metaConfig={metaConfig} />
       <Layout restricted={restricted}>{children}</Layout>
     </Fragment>
   )
