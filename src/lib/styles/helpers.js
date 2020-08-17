66309c8f09a381d4c44603c50105ba0c6a7fe58e
Suranart Niamcome <suranartnc@gmail.com>
2020-08-17 17:06:10 +0700

    Bring back Adaptive component

--- src/lib/styles/helpers.js
+++ src/lib/styles/helpers.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react'
-// import { useUA } from '@lib/userAgent'
 
 export const breakpoints = {
   md: '48em', // 768px
@@ -11,15 +10,16 @@ export function media(bp = 'lg') {
   return `@media (min-width: ${breakpoints[bp]})`
 }
 
-// export function Adaptive({ wide = null, narrow = null }) {
-//   const {
-//     device: { isMobile },
-//   } = useUA()
+export function Adaptive({ userAgent, desktop = null, mobile = null }) {
+  const isMobile = userAgent.device.isMobile
+  return isMobile ? mobile : desktop
+}
 
-//   return isMobile ? narrow : wide
-// }
-
-export function Responsive({ breakpoint = 'md', wide = null, narrow = null }) {
+export function Responsive({
+  breakpoint = 'md',
+  desktop = null,
+  mobile = null,
+}) {
   return (
     <Fragment>
       <div
@@ -29,7 +29,7 @@ export function Responsive({ breakpoint = 'md', wide = null, narrow = null }) {
             display: 'none',
           },
         }}>
-        {narrow}
+        {mobile}
       </div>
       <div
         css={{
@@ -38,7 +38,7 @@ export function Responsive({ breakpoint = 'md', wide = null, narrow = null }) {
             display: 'block',
           },
         }}>
-        {wide}
+        {desktop}
       </div>
     </Fragment>
   )
--- src/lib/styles/index.js
+++ src/lib/styles/index.js
@@ -1,8 +1,4 @@
 // import './fontAwesome'
 
 export { default as GlobalStyles } from './GlobalStyles'
-export {
-  media,
-  breakpoints,
-  Responsive, //  Adaptive
-} from './helpers'
+export { media, breakpoints, Responsive, Adaptive } from './helpers'
