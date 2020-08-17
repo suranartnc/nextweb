aca263c8764de472a2bf44460a27ac595ef0b7c6
Suranart Niamcome <suranartnc@gmail.com>
2020-08-17 17:54:16 +0700

    Move custom store to features folder

--- src/lib/store/rootStore.js
+++ src/lib/store/rootStore.js
@@ -1,13 +1,10 @@
 import { configure } from 'mobx'
-
-import UIStore from '@features/_ui/store'
 import ErrorStore from '@features/_error/store'
 
 configure({ enforceActions: 'observed' })
 
 export default class RootStore {
   constructor() {
-    this.uiStore = new UIStore(this)
     this.errorStore = new ErrorStore(this)
   }
 }
