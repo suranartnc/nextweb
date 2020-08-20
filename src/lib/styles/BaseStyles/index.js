c0f55df5ea15a3146c79f791e2b73d053c29b861
Suranart Niamcome <suranartnc@gmail.com>
2020-08-20 16:12:59 +0700

    Disable Font Awesome by default

--- src/lib/styles/BaseStyles/index.js
+++ src/lib/styles/BaseStyles/index.js
@@ -6,7 +6,7 @@ import getGlobalStyles from '@modules/_ui/globalStyles'
 import normalize from './_normalize'
 import reset from './_reset'
 import clearFix from './_clearFix'
-import './_fontAwesome'
+// import './_fontAwesome'
 
 const getBaseStyles = colors => css`
   ${normalize}
