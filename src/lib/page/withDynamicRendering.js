6cf9deb013198938300488f676b2e278a04c969d
Suranart Niamcome <suranartnc@gmail.com>
2020-08-24 11:29:25 +0700

    Bring back with dyanamic renderinngg

--- src/lib/page/withDynamicRendering.js
+++ src/lib/page/withDynamicRendering.js
@@ -1,14 +1,11 @@
 import { flowRight as compose } from 'lodash'
 
-// import { withUA } from '@lib/userAgent'
+import { withUA } from '@lib/userAgent'
 import { withErrorHandling } from '@lib/error'
 
 export default function withDynamicRendering(options = {}) {
   return function(Component) {
-    const hocs = [
-      // withUA,
-      // withErrorHandling,
-    ]
+    const hocs = [withUA, withErrorHandling]
 
     return compose(...hocs)(Component)
   }
