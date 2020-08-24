4206344c962539f03fb645014ce8be10edd3207f
Suranart Niamcome <suranartnc@gmail.com>
2020-08-24 14:46:16 +0700

    Preserve auth session using cookie

--- src/lib/auth/index.js
+++ src/lib/auth/index.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect, useContext } from 'react'
+import { parseCookies, setCookie, destroyCookie } from 'nookies'
 import { useRouter } from 'next/router'
+import jwtDecode from 'jwt-decode'
+
 import { getAsPathByRouteName } from '@lib/router/utils'
-import { getDataFromToken } from '@modules/_auth/useAuth'
+
+import { AUTH_COOKIE_NAME, AUTH_COOKIE_MAX_AGE } from '@modules/_auth/constants'
 
 export const userContext = React.createContext({})
 
 export function useMember() {
   return useContext(userContext)
+}
+
+function getDataFromToken(token) {
+  if (token === null || token === false) return ''
+
+  return jwtDecode(token)
 }
 
 const defaultUserData = {
@@ -19,9 +29,51 @@ export function AuthProvider({ children }) {
   const router = useRouter()
   const [userData, setUserData] = useState(defaultUserData)
 
+  // Initialize user data
   useEffect(() => {
-    console.log('userData', userData)
-  }, [userData])
+    const cookies = parseCookies()
+    let tokenInCookie = cookies[AUTH_COOKIE_NAME]
+
+    // Auto login if the user has token in cookie
+    if (tokenInCookie !== undefined) {
+      signInWithToken(tokenInCookie)
+    } else {
+      setUserData({
+        ...defaultUserData,
+        isAuthenticated: false,
+      })
+    }
+  }, [])
+
+  // Save token to cookie after sign in
+  useEffect(() => {
+    const { isAuthenticated, token } = userData
+
+    const cookies = parseCookies()
+    let tokenInCookie = cookies[AUTH_COOKIE_NAME]
+
+    if (isAuthenticated === true && token !== null && !tokenInCookie) {
+      const payload = getDataFromToken(token)
+      const expires =
+        payload?.exp || Math.floor(Date.now() / 1000) + AUTH_COOKIE_MAX_AGE
+
+      setCookie(null, AUTH_COOKIE_NAME, token, {
+        path: '/',
+        expires: new Date(expires * 1000),
+      })
+    }
+  }, [userData.isAuthenticated])
+
+  // Clear auth cookie after sign out
+  useEffect(() => {
+    const { isAuthenticated } = userData
+
+    if (isAuthenticated === false) {
+      parseCookies()
+      destroyCookie(null, AUTH_COOKIE_NAME, { path: '/' })
+      router.push(getAsPathByRouteName('auth-login'))
+    }
+  }, [userData.isAuthenticated])
 
   const signInWithToken = token => {
     setUserData({
@@ -32,8 +84,7 @@ export function AuthProvider({ children }) {
   }
 
   const signOut = () => {
-    setUserData(defaultUserData)
-    router.push(getAsPathByRouteName('auth-login'))
+    setUserData({ ...defaultUserData, isAuthenticated: false })
   }
 
   return (
